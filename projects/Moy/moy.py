dbe7a48fef0d27b1539a3dd2b909bae34d37cc1f
daisseur <daisseur@gmail.com>
2023-09-01 22:21:57 +0200

    Linux Fix

--- projects/Moy/moy.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-def calc(notes):
-	notes = [float(i) for i in notes.split()]
-	total = 0
-	for i in notes:
-		total += i
-	print(total/len(notes))
-calc(input("Entrez vos notes séparé par un espace : "))
-sleep(10)
--- update.bat
+++ update.bat
@@ -1,4 +1,5 @@
 @echo off
 git add .
+git add --rename D:\projects\Public projects
 git commit -m "%*"
 git push -u

2366bf125c4c9d4b491a1e47490567b5d943afe6
agmbk <109393672+agmbk@users.noreply.github.com>
2023-09-02 03:19:56 +0200

    chore(discord-media): build chunk, fix

--- projects/Chrome-Extension/Discord-media-typescript/vite.config.ts
+++ projects/Chrome-Extension/Discord-media-typescript/vite.config.ts
@@ -1,11 +1,23 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import makeManifest from './utils/plugins/make-manifest';
 
 const root = resolve(__dirname, 'src');
 const pagesDir = resolve(root, 'pages');
 const assetsDir = resolve(root, 'assets');
 const outDir = resolve(__dirname, 'dist');
 const publicDir = resolve(__dirname, 'public');
+
+const pages = {
+	background : resolve(pagesDir, 'background', 'index.ts'),
+	 content : resolve(pagesDir, 'content', 'index.ts'),
+	 popup : resolve(pagesDir, 'popup', 'index.html'),
+}
+
+// devtools: resolve(pagesDir, 'devtools', 'index.html'),
+// panel: resolve(pagesDir, 'panel', 'index.html'),
+// newtab: resolve(pagesDir, 'newtab', 'index.html'),
+// options: resolve(pagesDir, 'options', 'index.html'),
 
 export default defineConfig({
 	resolve: {
@@ -15,7 +27,7 @@ export default defineConfig({
 			'~pages': pagesDir,
 		},
 	},
-	// plugins: [makeManifest(), buildContentScript()],
+	plugins: [makeManifest()],// buildContentScript()],
 	publicDir,
 	build: {
 		outDir,
@@ -23,16 +35,16 @@ export default defineConfig({
 		emptyOutDir: true,
 		rollupOptions: {
 			input: {
-				// devtools: resolve(pagesDir, 'devtools', 'index.html'),
-				// panel: resolve(pagesDir, 'panel', 'index.html'),
-				background: resolve(pagesDir, 'background', 'index.ts'),
-				content: resolve(pagesDir, 'content', 'index.ts'),
-				popup: resolve(pagesDir, 'popup', 'index.html'),
-				// newtab: resolve(pagesDir, 'newtab', 'index.html'),
-				// options: resolve(pagesDir, 'options', 'index.html'),
+				...pages
 			},
 			output: {
 				entryFileNames: (chunk) => `src/pages/${chunk.name}/index.js`,
+				manualChunks: {
+					...Object.entries(pages).reduce((acc, [key, value]) => {
+						acc[key] = [value];
+						return acc;
+					}, {}),
+				},
 			},
 		},
 	},
